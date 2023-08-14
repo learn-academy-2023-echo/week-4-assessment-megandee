23d1a99822d52e9026640532aa653475fbc1e12a
megandee <meganderisi@gmail.com>
2023-08-14 10:11:36 -0500

    week 4 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,43 +15,144 @@
 // a) Create a test with an expect statement using the variable provided.
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+// describe("shuffleContent", () => {
+//   it("removes the first item from the array and shuffles the remaining content", () => {
+//     const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+//     const colors2 = [
+//       "chartreuse",
+//       "indigo",
+//       "periwinkle",
+//       "ochre",
+//       "aquamarine",
+//       "saffron"
+//     ]
+//     expect(shuffleContent(colors1)).not.toEqual(expect.arrayContaining(expected))
+//     expect(shuffleContent(colors2)).not.toEqual(expect.arrayContaining(expected))
+//   })
+// })
+
+// const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = [
-  "chartreuse",
-  "indigo",
-  "periwinkle",
-  "ochre",
-  "aquamarine",
-  "saffron"
-]
+// const colors2 = [
+//   "chartreuse",
+//   "indigo",
+//   "periwinkle",
+//   "ochre",
+//   "aquamarine",
+//   "saffron"
+// ]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+// FAIL  ./code-challenges.test.js
+// shuffleContent
+//   ✕ removes the first item from the array and shuffles the remaining content
+// ● shuffleContent › removes the first item from the array and shuffles the remaining content
+//   ReferenceError: shuffleContent is not defined
+
+// Pseudo code:
+// input: array
+// output: array with first item removed, then shuffles remaining items
+// process: 
+// iteration is needed, maybe for loop? or maybe .filter() ?
+// first remove first item of array, can use .shift() as it removes value at the first index.
+// Math.random() arrays index and not the value.
+
 
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+// const shuffleContent = (array) => {
+//   let newArr = []
+//   for ( let i=0; i < array.length; i++){
+//     newArr.shift(array[i].Math.random())
+//   }
+//   return newArr
+// }
+
+// console.log(shuffleContent(colors1, colors2))
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const votes1 = { upVotes: 13, downVotes: 2 }
+// describe("endTally", () => {
+//   it("takes in an object that contains up votes and down votes and returns the end tally", () => {
+//     const votes1 = { upVotes: 13, downVotes: 2 }
+//     const votes2 = { upVotes: 2, downVotes: 33 }
+//     expect(endTally(votes1)).toEqual("11")
+//     expect(endTally(votes2)).toEqual("-31")
+//   })
+// })
+
+// const votes1 = { upVotes: 13, downVotes: 2 }
 // Expected output: 11
-const votes2 = { upVotes: 2, downVotes: 33 }
+// const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
+
+// FAIL  ./code-challenges.test.js
+// endTally
+//   ✕ takes in an object that contains up votes and down votes and returns the end tally (1 ms)
+// ● endTally › takes in an object that contains up votes and down votes and returns the end tally
+// ReferenceError: endTally is not defined
+
+// Pseudo code:
+// input: object, 2 key:value pairs
+// output: end tally
+// process: subtract the value of the keys
+        // dot notation can call on value
+        // can it subtract though?
+        // going deeper into classes.. maybe use a constructor
+        // am I overthinking this?
 
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+// const votes1 = { upVotes: 13, downVotes: 2 }
+// const votes2 = { upVotes: 2, downVotes: 33 }
+
+// const endTally = () => {
+
+// }
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const dataTypesArray1 = ["array", "object", "number", "string", "Boolean"]
-const dataTypesArray2 = ["string", "null", "Boolean", "string", "undefined"]
+// describe("noDupes", () => {
+//   it("returns one array with no duplicate values", () => {
+//     const dataTypesArray1 = ["array", "object", "number", "string", "Boolean"]
+//     const dataTypesArray2 = ["string", "null", "Boolean", "string", "undefined"]
+//     expect(noDupes(dataTypesArray1)).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
+//     expect(noDupes(dataTypesArray2)).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
+//   })
+// })
 // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+
+// FAIL  ./code-challenges.test.js
+// noDupes
+//   ✕ returns one array with no duplicate values (1 ms)
+// ● noDupes › returns one array with no duplicate values
+// ReferenceError: noDupes is not defined
+
+// Pseudo code:
+// input: 2 arrays
+// output: 1 array
+// process: iteration to go through arrays
+// .filter() will filter through and return a subset, 
+// logical operator 
+// concat would join but stretch wants spread ...
+
+// const noDupes = (arr1, arr2) => {
+  // when using the .concat() I recieve and undefined w/the console
+  //  let newArr = arr1.concat(arr2)
+   // when attempting the spread I receive newArr is not defined
+  // let combined = [...arr1, ...arr2] 
+//   let output = []
+//   output = newArr.filter((value) =>{
+//   return output 
+//   })
+// }
+// console.log(noDupes(dataTypesArray1, dataTypesArray2))
 
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+// const dataTypesArray1 = ["array", "object", "number", "string", "Boolean"]
+// const dataTypesArray2 = ["string", "null", "Boolean", "string", "undefined"]
+// Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,10 +10,37 @@ reposts2 = 42
 reposts3 = 221
 # Expected output: '221 is odd'
 
-# Pseudo code:
+# Pseudo code: 
+# input: integers
+# output: a string
+# process: A conditional to test, 
+# 'num % 2 == 0' means number is even
+# string interpolation in the implicit return, 
+
+
+# def even_or_odd num 
+#     if num % 2 == 0
+#       "#{num} is even"
+#     else 
+#       "#{num} is odd" 
+#     end 
+# end 
+# p even_or_odd(reposts1)
+# # "7 is odd"
+# p even_or_odd(reposts2)
+# # "42 is even"
+# p even_or_odd(reposts3)
+# "221 is odd"
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
 # HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
+
+# Pseudo code:
+# input: a string
+# output: string minus the vowels
+# process: .delete will return the string with characters in the intersection deleted
+# CRUD and hashes comes to mind
+# 
 
 beatles_album1 = 'Rubber Soul'
 # Expected output: 'Rbbr Sl'
@@ -22,9 +49,36 @@ beatles_album2 = 'Sgt Pepper'
 beatles_album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
-# Pseudo code:
+
+# # def remove_vowels str
+# #     if str == str.delete("aeiou")
+# #         'str'
+# #     else
+# #         'str'
+# #     end
+# # end
+# p beatles_album1(remove_vowels)
+# undefined local variable or method `remove_vowels' for main:Object (NameError)
+# p beatles_album1(remove_vowels)
+#                  ^^^^^^^^^^^^^
+# perhaps I overthought this one.
+
+
+# p beatles_album1.delete("aieou")
+# "Rbbr Sl"
+# p beatles_album2.delete("aeiou")
+# "Sgt Pppr"
+# p beatles_album3.delete("aeiou")
+# "Abby Rd"
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
+# Pseudo code:
+# input: a string
+# output: the same string but backwards and a boolean value
+# process: will need to uppercase all letters with .upcase, 
+    ## i cannot seem to figure out the appropriate placement for .upcase to work as designed. Tried after .reverse on line 92 first, then after is_palin on line 91.
+# then .reverse() to test, 
+#a conditional to perform the test
 
 palindrome_test_case1 = 'Racecar'
 # Expected output: 'Racecar is a palindrome'
@@ -32,5 +86,18 @@ palindrome_test_case2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
 palindrome_test_case3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+palindrome_test_case4 = 'racecar' #added to test if method would work with already lowercased letters, it did.
 
-# Pseudo code:
+# def is_palin str
+#     if str == str.reverse 
+#         "#{str} is a palindrome"
+#     else
+#         "#{str} is not a palindrome"
+#     end
+# end
+
+
+# p is_palin(palindrome_test_case2)
+# palindrome_test_case2 = 'LEARN'
+# p is_palin(palindrome_test_case4)
+# "racecar is a palindrome"
